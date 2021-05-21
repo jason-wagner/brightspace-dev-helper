2f9ff0419e4cc8993ce0da77f76c70dfc5702561
Jason Wagner <jason.w.wagner@gmail.com>
2021-05-21 01:13:39 -0400

    Relocate sdk files

--- init.php
+++ init.php
@@ -1,6 +1,6 @@
 <?php
 
 require_once __DIR__.'/vendor/autoload.php';
-require_once __DIR__.'/valence/D2LAppContextFactory.php';
+require_once __DIR__ . '/valence-sdk/D2LAppContextFactory.php';
 
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__)->load();
