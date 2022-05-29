c7ce375d5c075e8fe27454bbed57a2fc2ec88b28
Jason Wagner <jason.w.wagner@gmail.com>
2022-05-28 23:56:54 -0400

    Remove properties only in unstable version of API

--- src/Blocks/Role.php
+++ src/Blocks/Role.php
@@ -9,13 +9,4 @@ class Role extends Block
 	public string $Identifier;
 	public string $DisplayName;
 	public string $Code;
-	public string $Description;
-	public string $RoleAlias;
-	public bool $IsCascading;
-	public bool $AccessFutureCourses;
-	public bool $AccessInactiveCourses;
-	public bool $AccessPastCourses;
-	public bool $ShowInGrades;
-	public bool $ShowInUserProgress;
-	public bool $InClassList;
 }
