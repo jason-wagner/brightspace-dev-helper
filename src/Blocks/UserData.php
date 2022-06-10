a5f07343e45f41f432cf3f25824db23815006752
Jason Wagner <jason.w.wagner@gmail.com>
2022-06-10 14:23:38 -0400

    Implement retrieval of Datahub record for course offerings

--- src/Blocks/UserData.php
+++ src/Blocks/UserData.php
@@ -2,11 +2,15 @@
 
 namespace BrightspaceDevHelper\Valence\Block;
 
+use BrightspaceDevHelper\DataHub\Model\User;
+use BrightspaceDevHelper\Valence\Client\Valence;
+use BrightspaceDevHelper\Valence\Object\DateTime;
+use BrightspaceDevHelper\Valence\Object\NotInDatahub;
 use BrightspaceDevHelper\Valence\Structure\Block;
 
 class UserData extends Block
 {
-	public int $OrgId;
+	public int|NotInDatahub $OrgId;
 	public int $UserId;
 	public string $FirstName;
 	public ?string $MiddleName;
@@ -14,10 +18,10 @@ class UserData extends Block
 	public string $UserName;
 	public ?string $ExternalEmail;
 	public ?string $OrgDefinedId;
-	public string $UniqueIdentifier;
+	public string|NotInDatahub $UniqueIdentifier;
 	public UserActivationData $Activation;
 	public ?string $LastAccessedDate;
-	public string $Pronouns;
+	public string|NotInDatahub|null $Pronouns;
 
 	public function __construct(array $response)
 	{
@@ -25,4 +29,23 @@ class UserData extends Block
 		$this->Activation = new UserActivationData($response['Activation']);
 	}
 
+	public static function fromDatahub(User $record, Valence $valence): UserData
+	{
+		$a = [
+			'OrgId' => new NotInDatahub(),
+			'UserId' => $record->UserId,
+			'FirstName' => $record->FirstName,
+			'MiddleName' => $record->MiddleName,
+			'LastName' => $record->LastName,
+			'UserName' => $record->UserName,
+			'ExternalEmail' => $record->ExternalEmail,
+			'OrgDefinedId' => $record->OrgDefinedId,
+			'UniqueIdentifier' => new NotInDatahub(),
+			'Activation' => ['IsActive' => (bool)$record->IsActive],
+			'LastAccessedDate' => DateTime::createFromTimestamp($record->LastAccessed, $valence)->getIso8601(),
+			'Pronouns' => new NotInDatahub()
+		];
+
+		return new UserData($a);
+	}
 }
